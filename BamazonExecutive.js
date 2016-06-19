8beea2b6eb5433bc0580c92e134fb7fc56326148
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-06-19 15:50:43 -0400

    Bamazon Executive inital files

---
+++ BamazonExecutive.js
@@ -0,0 +1,27 @@
+function bamazonExec() {
+	
+	// npm variable declarations
+
+	// use mysql npm
+	var mysql = require('mysql');
+
+	// set connection for mysql
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: 'hihi',
+		database: 'Bamazon'
+	});
+
+	// use inquirer npm
+	var inquirer = require('inquirer');
+	// store the inquirer prompt in a variable as per the npm inquirer docs so we don't affect other libraries that also rely on inquirer when new prompt types are added or overwritten
+	var prompt = inquirer.createPromptModule();
+
+
+	// functions below
+
+} // end bamazonExec()
+
+bamazonExec();
