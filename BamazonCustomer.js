5da6e4fb734634305c8fc61ee95694fa8c9c4200
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-06-19 10:29:07 -0400

    displaying product id, name and price

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,5 +1,7 @@
 function bamazonCust() {
 
+	// npm variable declarations
+
 	// use mysql npm
 	var mysql = require('mysql');
 
@@ -14,17 +16,46 @@ function bamazonCust() {
 
 	// use inquirer npm
 	var inquirer = require('inquirer');
+	// store the inquirer prompt in a variable as per the npm inquirer docs so we don't affect other libraries that also rely on inquirer when new prompt types are added or overwritten
+	var prompt = inquirer.createPromptModule();
 
-	// connection to mysql server
-	connection.query('SELECT * FROM Products', function(err, data) {
 
-		// if error, throw error
-		if (err) throw err;
+	// functions below
 
-		// welcome screen and display all the items available to buy
-		console.log('Welcom to the Bamazon Cult Cinema Shop. Where you can purchase classic films in classic formats.');
-		console.log(data);
-	});
+	// welcome the user and display products to the screen
+	function displayProducts() {
+		
+		// connection to mysql server
+		connection.query('SELECT * FROM Products', function(err, data) {
+
+			// if error, throw error
+			if (err) throw err;
+
+			// welcome screen and display all the items available to buy
+			console.log('Welcom to the Bamazon Cult Cinema Shop. Where you can purchase classic films in classic formats. \n');
+
+			// loop through the list of items and display to the screen
+			var i;
+			var data_length = data.length;
+			for (i = 0; i < data_length; i++) {
+
+				// display the item ID, name of product and price
+				console.log(data[i].ItemID + ' ' + data[i].ProductName + ' $' + data[i].Price + "\n");
+
+			} // end for loop
+			
+		}); // end connection.query()
+
+	} // end displayProducts()
+
+	// ask user which product they would like to buy
+	function selectProduct() {
+		
+
+
+	} // end selectProduct()
+
+	displayProducts();
 	
 }
 
